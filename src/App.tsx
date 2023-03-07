793e43ed2d006f336cff5a7466d7959bf09c582c
Paulo Fernandes <paulor.exe@gmail.com>
2023-03-07 16:32:45 -0300

    Criação da tabela de exibição de despesas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { useState, useEffect } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { items } from './data/items';
+import { categories } from './data/categories';  
+import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter'
+import { TableArea } from './components/TableArea'
 
 const App = () => {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(()=>{
+    setFilteredList( filterListByMonth(list, currentMonth) );
+  }, [list, currentMonth])
+
   return (
     <C.Container>
       <C.Header>
@@ -13,7 +28,7 @@ const App = () => {
         
         {/* Área de inserção */}
 
-        {/* Tabela de itens */}
+        <TableArea list={filteredList} />
 
       </C.Body>
     </C.Container>
