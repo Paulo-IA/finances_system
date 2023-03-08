e7a9518d8e8fb112dc3faad864e8aa5b91efa379
Paulo Fernandes <paulor.exe@gmail.com>
2023-03-08 13:04:29 -0300

    Criação de mudança de mês

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { items } from './data/items';
 import { categories } from './data/categories';  
 import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter'
 import { TableArea } from './components/TableArea'
+import { InfoArea } from './components/InfoArea';
 
 const App = () => {
   const [list, setList] = useState(items);
@@ -16,6 +17,10 @@ const App = () => {
     setFilteredList( filterListByMonth(list, currentMonth) );
   }, [list, currentMonth])
 
+  const handleMonthChange = ( newMonth: string ) => {
+    setCurrentMonth(newMonth)
+  }
+
   return (
     <C.Container>
       <C.Header>
@@ -24,7 +29,10 @@ const App = () => {
 
       <C.Body>
 
-        {/* Área de informações */}
+        <InfoArea
+          currentMonth={currentMonth}
+          onMonthChange={handleMonthChange}
+        />
         
         {/* Área de inserção */}
 
